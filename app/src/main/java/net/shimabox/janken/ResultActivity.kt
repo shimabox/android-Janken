e75aaff5ead9b3a033889e312f533fa629b66a2e
shimabox <shimabox@gmail.com>
2020-12-24 00:07:06 +0900

    :+1: コンピュータの出す手を実装したものに置き換え、じゃんけんの結果を保存する

--- app/src/main/java/net/shimabox/janken/ResultActivity.kt
+++ app/src/main/java/net/shimabox/janken/ResultActivity.kt
@@ -36,7 +36,7 @@ class ResultActivity : AppCompatActivity() {
         }
 
         // コンピュータの手を決める
-        val comHand = (Math.random() * 3).toInt()
+        val comHand = getHand()
         when (comHand) {
             gu -> binding.comHandImage.setImageResource(R.drawable.com_gu)
             choki -> binding.comHandImage.setImageResource(R.drawable.com_choki)
@@ -53,6 +53,9 @@ class ResultActivity : AppCompatActivity() {
 
         // 戻る
         binding.backButton.setOnClickListener { finish() }
+
+        // じゃんけんの結果を保存する
+        saveData(myHand, comHand, gameResult)
     }
 
     private fun saveData(myHand: Int, comHand: Int, gameResult: Int) {

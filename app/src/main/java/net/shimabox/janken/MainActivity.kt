438ca72abb8bb57a195be9c383c559407bbf8af9
shimabox <shimabox@gmail.com>
2020-12-23 22:55:00 +0900

    :sparkles: インテントを使って画面遷移

--- app/src/main/java/net/shimabox/janken/MainActivity.kt
+++ app/src/main/java/net/shimabox/janken/MainActivity.kt
@@ -1,11 +1,26 @@
 package net.shimabox.janken
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import net.shimabox.janken.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gu.setOnClickListener { onJankenButtonTapped(it) }
+        binding.choki.setOnClickListener { onJankenButtonTapped(it) }
+        binding.pa.setOnClickListener { onJankenButtonTapped(it) }
+    }
+
+    fun onJankenButtonTapped(view: View?) {
+        var intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
     }
 }
